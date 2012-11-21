48a68bcf95599372bb777ff3f8ca88c05292fa30
Dmitry E. Oboukhov <unera@debian.org>
2012-11-21 14:22:15 +0400

    some tests for net-split

--- t/test-data/init.lua
+++ t/test-data/init.lua
@@ -3,6 +3,17 @@ function test_parallel(delay, id)
     return id
 end
 
+function test_parallel_big_tuple(delay, id, ...)
+    local args = { ... }
+
+    local size = 0
+    for i, v in pairs(args) do
+        size = size + string.len(v)
+    end
+    box.fiber.sleep(delay)
+    return { id, tostring(size) }
+end
+
 
 function test_return_one()
     return { 'one' }
