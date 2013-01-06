2a65d8a8b9d0d70650b3e53dcf71eb5b02d5e144
Dmitry E. Oboukhov <unera@debian.org>
2013-01-06 19:21:36 +0400

    bump module version, too

--- lib/DR/Tarantool.pm
+++ lib/DR/Tarantool.pm
@@ -76,7 +76,7 @@ our %EXPORT_TAGS = (
 our @EXPORT_OK = ( map { @$_ } values %EXPORT_TAGS );
 $EXPORT_TAGS{all} = \@EXPORT_OK;
 our @EXPORT = @{ $EXPORT_TAGS{client} };
-our $VERSION = '0.24';
+our $VERSION = '0.25';
 
 
 =head1 EXPORT
