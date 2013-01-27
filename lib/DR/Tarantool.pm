98cd4f4455a1c61a1874d46dc03dbc3c0914d09d
Dmitry E. Oboukhov <unera@debian.org>
2013-01-27 16:56:00 +0400

    fix VCS-link

--- lib/DR/Tarantool.pm
+++ lib/DR/Tarantool.pm
@@ -183,7 +183,7 @@ L<DR::Tarantool::AsyncClient>.
 =head1 VCS
 
 The project is placed git repo on github:
-L<https://github.com/unera/dr-tarantool/>.
+L<https://github.com/dr-co/dr-tarantool/>.
 
 =cut
 
